110cb8e2f4513912eb63f80fdba806f04b9977f3
vishalchaware <vishalchaware144@gmail.com>
2021-05-15 16:39:56 +0530

    mongoose connect

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
+// Import module
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
+// Initialize app
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+// Dev logging middleware
+if (process.env.NODE_ENV !== 'development') {
+	app.use(morgan('dev'));
+}
 
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+// start applications
+const PORT = process.env.PORT || 5000;
 app.listen(
 	PORT,
 	console.log(
-		`Server running in ${process.env.Node_ENV} mode on port ${PORT}`
+		`Server running in ${process.env.Node_ENV} mode on port ${PORT}`.yellow
+			.bold
 	)
 );
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${(err, message)}`.red);
+	// Close server & exit process
+	server.close(() => process.exit(1));
+});
