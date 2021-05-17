fd59b48cabad3f1132d5a705a0ef9a570fa33aa5
vishalchaware <vishalchaware144@gmail.com>
2021-05-17 12:45:44 +0530

    Select, sort & pagination

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,16 +9,53 @@ const Bootcamp = require('../models/bootcamp');
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
 	let query;
 
-	let queryStr = JSON.stringify(req.query);
+	// Copy req.query
+	const reqQuery = { ...req.query };
 
+	// Fields to exclude
+	const removeFields = ['select', 'sort', 'page', 'limit'];
+
+	// Loop over removeFields and delete them from reqQuery
+	removeFields.forEach((param) => delete reqQuery[param]);
+
+	console.log(req.query);
+
+	// Create query string
+	let queryStr = JSON.stringify(reqQuery);
+
+	// Create operators ($gt, $gte, etc)
 	queryStr = queryStr.replace(
 		/\b(gt|gte|lt|lte|in)\b/g,
 		(match) => `$${match}`
 	);
 
+	// Finding resource
 	query = Bootcamp.find(JSON.parse(queryStr));
 
+	// Select Fields
+	if (req.query.select) {
+		const fields = req.query.select.split(',').join(' ');
+		query = query.select(fields);
+	}
+
+	// Sort
+	if (req.query.sort) {
+		const sortBy = req.query.sort.split(',').join(' ');
+		query = query.sort(sortBy);
+	} else {
+		query = query.sort('-createdAt');
+	}
+
+	// Pagination
+	const page = parseInt(req.query.page, 10) || 1;
+	const limit = parseInt(req.query.limit, 10) || 25;
+	const startIndex = (page - 1) * limit;
+	const endIndex = page * limit;
+	const total = await Bootcamp.countDocuments();
+
+	// Executing query
 	const bootcamps = await query;
+
 	res.status(200).json({
 		success: true,
 		count: bootcamps.length,
