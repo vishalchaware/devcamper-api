85f2f463333a2d06fe2ccd4d2822ace7ef6cd4d8
vishalchaware <vishalchaware144@gmail.com>
2021-05-20 14:26:11 +0530

    Admin CRUD

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -10,13 +10,14 @@ const {
 } = require('../controllers/bootcamps');
 
 const Bootcamp = require('../models/Bootcamp');
-const advancedResults = require('../middleware/advancedResults');
 
 // Include other resource routers
 const courseRouter = require('./courses');
 
 const router = express.Router();
 
+// Middleware
+const advancedResults = require('../middleware/advancedResults');
 const { protect, authorize } = require('../middleware/auth');
 
 // Re-route into other resource routers
